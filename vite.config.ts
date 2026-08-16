430c29f3a7a5ba439c454b5f507a45691450466a
saga7o-svg <saga7o@gmail.com>
2026-08-17 00:25:29 +0700

    fix: set target vercel in tanstackStart plugin config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 export default defineConfig({
   plugins: [
     tanstackStart({
+      target: "vercel",
       server: { entry: "server" },
     }),
     react(),
